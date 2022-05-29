0ae1c88b62ce913c43c3d93ad9432d0aea0388d2
Marcelo Alves <alvesmarcelocf@proton.me>
2022-05-29 10:59:05 -0400

    Updated catalyst tool script.

--- files/catalyst.sh
+++ files/catalyst.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -x
 
-mkdir -p /var/tmp/catalyst/builds/clang-musl
-wget https://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-musl/stage3-amd64-musl-20220501T170547Z.tar.xz \
-    -O /var/tmp/catalyst/builds/clang-musl/stage3-amd64-musl-latest.tar.xz
 catalyst -s latest
 catalyst -f /stage1.spec
